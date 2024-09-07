8ef1da3d25cff25f9380793ba52ee63f0d6d60d9
Harshit <harshitkumarppnews9@gmail.com>
2024-09-07 10:02:58 +0530

    final changes

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -4,7 +4,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  root: 'frontend', // Set the root directory for your project
+  // root: '/', // Set the root directory for your project
   server: {
     port: 3000,
     // Get rid of the CORS error
@@ -15,9 +15,5 @@ export default defineConfig({
         secure: false,
       },
     },
-  },
-  build: {
-    outDir: '/dist', // Adjust this if you want to output the build to a different directory
-    emptyOutDir: true, // Clear the output directory before each build
   },
 });
